a4036318a90f4ee8dc15acea884f94a8f775a429
pandeng <pd.seeker@gmail.com>
2019-08-02 16:43:09 +0800

    App one finished

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -4,6 +4,7 @@ from .models import Topic
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from .forms import TopicForm, EntryForm
+from .models import Topic, Entry
 
 # Create your views here.
 
@@ -48,6 +49,21 @@ def new_entry(request, topic_id):
 
     context = {'topic':topic, 'form':form}
     return render(request, 'learning_logs/new_entry.html', context)
+
+def edit_entry(request, entry_id):
+    entry = Entry.objects.get(id=entry_id)
+    topic = entry.topic
+
+    if request.method != 'POST':
+        form = EntryForm(instance=entry)
+    else:
+        form = EntryForm(instance=entry, data=request.POST)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('learning_logs:topic', args=[topic.id]))
+
+    context = {'entry':entry, 'topic':topic, 'form':form}
+    return render(request, 'learning_logs/edit_entry.html',context)
 
 
 
